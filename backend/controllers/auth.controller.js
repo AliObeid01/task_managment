771851cbca5bbfec643ac1c74080df4e978168d1
unknown <aliiobeid01@gmail.com>
2023-08-08 18:02:49 +0300

    backend:create signup and signin functions in auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,44 @@
-const {Router} = require('express');
-const {signin, signup} = require('../controllers/auth.controller')
-const router = Router();
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post('/signin', signin);
-router.post('/signup', signup);
+const signup = async (req, res) => {
+  const { name, email, password} = req.body;
+    try {
+    
+      const user = new User();
+      user.name = name;
+      user.email = email;
+      user.password = await bcrypt.hash(password, 10);
+  
+      await user.save();
+      res.json(user);
+  
+    } catch(err) {
+      res.status(400).json({
+        message: err.message
+      });
+    }
+  }
 
-module.exports = router;
+const signin = async (req, res) => {
+  const {email, password} = req.body;
+
+  const user = await User.findOne({email});
+
+  if(!user) return res.status(404).json({message: "Invalid Credentials"});
+
+  const isMatch = bcrypt.compare(password, user.password);
+  if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+
+  const token = jwt.sign({email: user.email}, process.env.JWT_SECRET_KEY, {
+    expiresIn: '10h'
+  });
+
+  res.status(200).json(token)
+  }
+  
+module.exports = {
+  signin,
+  signup
+}
