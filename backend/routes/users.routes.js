0348ff109308431e8b789c683f8acda4061f9891
unknown <aliiobeid01@gmail.com>
2023-08-08 19:45:57 +0300

    backend:add the route of the getTasks function in user routes file

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const {getUser,addTask} = require('../controllers/user.controller');
+const {getUser,addTask,getTasks} = require('../controllers/user.controller');
 const authUserMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
 router.get('/user-profile', authUserMiddleware, getUser);
 router.post('/add_task', authUserMiddleware, addTask);
+router.get('/tasks', authUserMiddleware, getTasks);
 
 module.exports = router;
