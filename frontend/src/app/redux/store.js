96a3fa7620b2df3cd250bf2b0500c0ee67bc51de
unknown <aliiobeid01@gmail.com>
2023-08-09 20:40:13 +0300

    frontend:add tasksApi and TasksReducer to store file inside redux folder

--- frontend/src/app/redux/store.js
+++ frontend/src/app/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from './authApi';
+import { tasksApi } from './tasksApi';
+import tasksReducer from './tasksSlice';
 
 export const store = configureStore({
   reducer: {
+    tasks: tasksReducer,
+    [tasksApi.reducerPath]: tasksApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(tasksApi.middleware,authApi.middleware),
 
 });
