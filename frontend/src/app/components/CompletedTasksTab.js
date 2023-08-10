e6b85bbd4d8eaa385fe3160d05a7cb68038decda
unknown <aliiobeid01@gmail.com>
2023-08-10 12:50:08 +0300

    frontend:fetch the api for getCompletedTasks to display the completed tasks

--- frontend/src/app/components/CompletedTasksTab.js
+++ frontend/src/app/components/CompletedTasksTab.js
@@ -1,11 +1,46 @@
 "use client";
-import TaskItem from './TaskItem';
+import { useDeleteTaskMutation , useGetCompletedTasksMutation} from '../redux/tasksApi';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 const CompletedTasksTab = () => {
+  const [getCompletedTasks] = useGetCompletedTasksMutation();
+  const [deleteTask] = useDeleteTaskMutation();
+
+  const [tasks, setCompletedTasks] = useState([]);
+ 
+  useEffect(() => {
+    fetchCompletedTasks();
+  }, []);
+
+  const fetchCompletedTasks = async () => {
+    try {
+      const response = await getCompletedTasks();
+      setCompletedTasks(response.data.data.tasks);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  }  
+
+  const handleDeleteTask = async (task_id) => {   
+    try {
+      await deleteTask(task_id);
+      fetchCompletedTasks();
+    } catch (error) {
+      console.error('Delete task error:', error);
+    }
+  };
 
   return (
-    <div>
-      <p>complete</p>
+    <div className="task-item">
+      {tasks && tasks.map((task) => (
+        <div>
+        <h3>{task.title}</h3>
+        <p>{task.description}</p>
+        <p>Due: {task.due_date}</p>
+        <button onClick={() => handleDeleteTask(task._id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 };
